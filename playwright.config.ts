4d59a1ec7c5c6a94daae5212f1293da38533aed9
Santhosh <santhoshshetty58@gmail.com>
2024-01-27 08:53:50 +0100

    Headless set to true

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,20 @@
 import { PlaywrightTestConfig, defineConfig, devices } from "@playwright/test";
 
-const config: PlaywrightTestConfig={
-    testMatch: ["tests/sample.test.ts"],
-    workers:4,
-    fullyParallel: true,
-    use: {
-        baseURL: "https://ecommerce-playground.lambdatest.io/index.php?",
-        headless: false,
-        screenshot: "only-on-failure",
-        video: "retain-on-failure"
-    },
-    retries: 2,
-    reporter: [["dot"],["json",{outputFile: "jsonReports/jsonReport.json"}],["html",{open: "never"}]]
+const config: PlaywrightTestConfig = {
+  testMatch: ["tests/sample.test.ts"],
+  workers: 4,
+  fullyParallel: true,
+  use: {
+    baseURL: "https://ecommerce-playground.lambdatest.io/index.php?",
+    headless: true,
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+  },
+  retries: 2,
+  reporter: [
+    ["dot"],
+    ["json", { outputFile: "jsonReports/jsonReport.json" }],
+    ["html", { open: "never" }],
+  ],
 };
 export default config;
